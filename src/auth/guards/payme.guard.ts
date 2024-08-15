05ce25c64e84b82a44dc23b4d05e944f28a3e289
Bek Shoyatbek <92543629+bek-shoyatbek@users.noreply.github.com>
2024-08-15 11:13:01 +0500

    Kerakli kommentlar , tushunarli bo'lishi uchun qo'shildi.

--- src/auth/guards/payme.guard.ts
+++ src/auth/guards/payme.guard.ts
@@ -3,16 +3,19 @@ import { ConfigService } from '@nestjs/config';
 import { Request, Response } from 'express';
 import { PaymeError } from 'src/payme/constants/payme-error';
 
+
+// ! Payme dan kelayotgan so'rovlarni tekshirib va server xavfsizligini taminlash uchun
 @Injectable()
 export class PaymeBasicAuthGuard implements CanActivate {
   constructor(private configService: ConfigService) {}
-
+  
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest<Request>();
     const response = context.switchToHttp().getResponse<Response>();
     const token = this.extractTokenFromHeader(request);
     const transId = request?.body?.id;
 
+ // Paymega qaytadigan hatolar status kodi doim 200 bo'lishi lozim !
     if (!token) {
       response.status(200).send({
         id: transId,
@@ -34,6 +37,7 @@ export class PaymeBasicAuthGuard implements CanActivate {
 
       const isValidUsername =
         this.configService.get<string>('PAYME_LOGIN') === username;
+      // ! production mode da passwordni o'zgartishni unutmang
       const isValidPassword =
         this.configService.get<string>('PAYME_PASSWORD_TEST') === password;
 
